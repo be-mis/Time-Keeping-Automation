f2808e7625d2f6c68854fe04ebd4456adab06c21
Jonabols <jonathan.francisco@barbizonfashion.com>
2025-03-17 10:25:47 +0800

    Update urls.py

--- TIMEKEEPING/time_app/urls.py
+++ TIMEKEEPING/time_app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from . import views
+from . import views, printerlogs
 
 urlpatterns = [
     path('', views.upload_file, name="home-page"),
@@ -7,6 +7,8 @@ urlpatterns = [
     
     path('extract/<str:instance>/', views.extract_data, name="extract-data"),  # Added instance parameter
 
-    path('download/<int:pk>/', views.download_all, name="download-excel")
+    path('download/<str:instance>/', views.download_all, name="download-excel"),
+    path('epc-printer/', printerlogs.extract_print_data_epc, name='printer_logs_epc'),
+    path('nbfi-printer/', printerlogs.extract_print_data_nbfi, name='printer_logs_nbfi'),
 ]
 
